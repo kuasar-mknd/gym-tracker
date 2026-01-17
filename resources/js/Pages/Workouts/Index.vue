40b4f2f7c2a4c873150a3afd8e5826a5ec1fae7d
Samuel <samuel@mknd-records.com>
2026-01-16 22:15:09 -0500

    refactor(ui): remove redundant FAB button from Workouts/Index
    The black FAB button in the bottom-right corner was redundant because:
    - The central pink FAB in BottomNav already creates a new workout
    - The + button in the header also creates a new workout

--- resources/js/Pages/Workouts/Index.vue
+++ resources/js/Pages/Workouts/Index.vue
@@ -232,24 +232,5 @@ const formatDate = (dateStr) => {
                 </div>
             </div>
         </div>
-
-        <!-- FAB for mobile -->
-        <button
-            @click="createWorkout"
-            class="glass-fab sm:hidden"
-            :disabled="form.processing"
-            aria-label="Nouvelle séance"
-        >
-            <svg
-                class="h-6 w-6 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-            >
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />
-            </svg>
-        </button>
     </AuthenticatedLayout>
 </template>
