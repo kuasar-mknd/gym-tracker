85d76c884f72c31175135dc124c78f054d72c2a2
Samuel <samuel@mknd-records.com>
2026-02-11 17:33:41 -0500

    feat: add default exercises and backoffice installer action

--- app/Filament/Resources/Exercises/Pages/ListExercises.php
+++ app/Filament/Resources/Exercises/Pages/ListExercises.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace App\Filament\Resources\Exercises\Pages;
 
 use App\Filament\Resources\Exercises\ExerciseResource;
+use Filament\Actions\Action;
 use Filament\Actions\CreateAction;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Support\Facades\Artisan;
 
 class ListExercises extends ListRecords
 {
@@ -15,6 +18,22 @@ class ListExercises extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('installerParDefaut')
+                ->label('Installer Exercices par Défaut')
+                ->icon('heroicon-o-plus-circle')
+                ->color('info')
+                ->requiresConfirmation()
+                ->action(function (): void {
+                    Artisan::call('db:seed', [
+                        '--class' => 'PrecorExerciseSeeder',
+                        '--no-interaction' => true,
+                    ]);
+
+                    Notification::make()
+                        ->title('Exercices installés avec succès !')
+                        ->success()
+                        ->send();
+                }),
             CreateAction::make(),
         ];
     }
