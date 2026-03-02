128bb59663f31f0a348d0768b95586607a25ec42
Samuel <60432398+kuasar-mknd@users.noreply.github.com>
2026-03-02 14:06:09 -0500

    refactor: Extract logic from SetController (#612)
    Extract logic for creating sets into an Action class (`CreateSetAction`).
    Update `SetController::store` to use dependency injection to call the
    new action. Format files and sort imports.

---
+++ app/Actions/Workouts/CreateSetAction.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\Workouts;
+
+use App\Models\Set;
+use App\Models\User;
+use App\Models\WorkoutLine;
+use App\Services\StatsService;
+
+final class CreateSetAction
+{
+    public function __construct(protected StatsService $statsService)
+    {
+    }
+
+    /**
+     * Create a new set for a workout line.
+     *
+     * @param  array<string, mixed>  $data
+     */
+    public function execute(User $user, WorkoutLine $workoutLine, array $data): Set
+    {
+        $set = $workoutLine->sets()->create(
+            collect($data)->except('workout_line_id')->toArray()
+        );
+
+        $this->statsService->clearWorkoutRelatedStats($user);
+
+        return $set;
+    }
+}
