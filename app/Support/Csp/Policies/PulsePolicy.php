1cc56ebe3448d5ec04db5e650989cc4c5f9a9438
Samuel <samuel@mknd-records.com>
2026-03-02 19:14:29 -0500

    fix: correct PulsePolicy implementation to fix CSP 500 error
    - Changed PulsePolicy to extend Spatie\Csp\Presets\Basic.
    - Updated configuration to use addDirective and addNonceForDirective methods.
    - Fixes PHP-LARAVEL-5.

--- app/Support/Csp/Policies/PulsePolicy.php
+++ app/Support/Csp/Policies/PulsePolicy.php
@@ -6,27 +6,20 @@ namespace App\Support\Csp\Policies;
 
 use Spatie\Csp\Directive;
 use Spatie\Csp\Keyword;
-use Spatie\Csp\Policy;
-use Spatie\Csp\Preset;
+use Spatie\Csp\Presets\Basic;
 
-class PulsePolicy implements Preset
+class PulsePolicy extends Basic
 {
-    public function configure(Policy $policy): void
+    public function configure(): void
     {
-        $policy
-            ->add(Directive::BASE, Keyword::SELF)
-            ->add(Directive::CONNECT, Keyword::SELF)
-            ->add(Directive::DEFAULT, Keyword::SELF)
-            ->add(Directive::FORM_ACTION, Keyword::SELF)
-            ->add(Directive::FRAME, Keyword::SELF)
-            ->add(Directive::MEDIA, Keyword::SELF)
-            ->add(Directive::OBJECT, Keyword::NONE)
-            ->add(Directive::SCRIPT, [Keyword::SELF, Keyword::UNSAFE_EVAL])
-            ->addNonce(Directive::SCRIPT)
-            ->add(Directive::STYLE, Keyword::SELF)
-            ->addNonce(Directive::STYLE)
-            ->add(Directive::STYLE, 'https://fonts.bunny.net')
-            ->add(Directive::FONT, [Keyword::SELF, 'https://fonts.bunny.net'])
-            ->add(Directive::IMG, [Keyword::SELF, 'data:', 'https:']);
+        parent::configure();
+
+        $this
+            ->addDirective(Directive::SCRIPT, Keyword::UNSAFE_EVAL)
+            ->addNonceForDirective(Directive::SCRIPT)
+            ->addNonceForDirective(Directive::STYLE)
+            ->addDirective(Directive::STYLE, 'https://fonts.bunny.net')
+            ->addDirective(Directive::FONT, [Keyword::SELF, 'https://fonts.bunny.net'])
+            ->addDirective(Directive::IMG, [Keyword::SELF, 'data:', 'https:']);
     }
 }
