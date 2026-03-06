62ef84a08d3765d7d8fd87a4fdc54d57526c7153
kuasar-mknd <60432398+kuasar-mknd@users.noreply.github.com>
2026-03-06 06:18:04 +0000

    feat(backend): Implement CRUD for WorkoutTemplateSet
    Added standard CRUD methods for WorkoutTemplateSet in an API Controller, complete with form requests and authorization logic. Routes registered in routes/api.php.

---
+++ app/Policies/WorkoutTemplateSetPolicy.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\WorkoutTemplateSet;
+use App\Models\WorkoutTemplateLine;
+
+final class WorkoutTemplateSetPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, WorkoutTemplateSet $workoutTemplateSet): bool
+    {
+        return $user->id === $workoutTemplateSet->workoutTemplateLine->workoutTemplate->user_id;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user, ?WorkoutTemplateLine $workoutTemplateLine = null): bool
+    {
+        if ($workoutTemplateLine === null) {
+            return true;
+        }
+
+        return $user->id === $workoutTemplateLine->workoutTemplate->user_id;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, WorkoutTemplateSet $workoutTemplateSet): bool
+    {
+        return $user->id === $workoutTemplateSet->workoutTemplateLine->workoutTemplate->user_id;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, WorkoutTemplateSet $workoutTemplateSet): bool
+    {
+        return $user->id === $workoutTemplateSet->workoutTemplateLine->workoutTemplate->user_id;
+    }
+}
