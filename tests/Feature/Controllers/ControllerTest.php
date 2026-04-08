ae370f6934a1b59cb07e21fae0fa3e38e976f345
kuasar-mknd <60432398+kuasar-mknd@users.noreply.github.com>
2026-04-08 21:28:06 +0000

    test: Add Pest coverage for Controller

---
+++ tests/Feature/Controllers/ControllerTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+describe('Controller', function (): void {
+    describe('user', function (): void {
+        it('throws RuntimeException if user is not authenticated', function (): void {
+            Auth::shouldReceive('user')->once()->andReturn(null);
+
+            $controller = new class extends Controller {
+                public function getUser()
+                {
+                    return $this->user();
+                }
+            };
+
+            expect(fn () => $controller->getUser())
+                ->toThrow(\RuntimeException::class, 'User not authenticated');
+        });
+
+        it('returns user if authenticated', function (): void {
+            $user = User::factory()->make();
+            Auth::shouldReceive('user')->once()->andReturn($user);
+
+            $controller = new class extends Controller {
+                public function getUser()
+                {
+                    return $this->user();
+                }
+            };
+
+            expect($controller->getUser())->toBe($user);
+        });
+    });
+});
